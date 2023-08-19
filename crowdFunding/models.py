d8385473a01239cc0c488e42e6500837df7f5c93
neelansh27 <neelanshsharma30@gmail.com>
2023-08-19 09:38:04 +0530

    Updated Fund model

--- crowdFunding/models.py
+++ crowdFunding/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.utils.text import slugify
-import uuid 
+import uuid
+
+from django.utils import timezone 
 
 class Fund(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     content = models.TextField()
     goal = models.IntegerField()
+    amount_gained = models.IntegerField(default=0)
+    status= models.CharField(max_length=10,default="In Progress")
     slug = models.SlugField(unique=True)
+    event_datetime=models.DateTimeField()
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -19,6 +24,9 @@ class Fund(models.Model):
             f"title:{self.title},"
             f"content:{self.content},"
             f"goal:{self.goal},"
+            f"Amount Gained:{self.amount_gained},"
+            f"Status:{self.status},"
+            f"time:{self.event_datetime},"
             f"slug:{self.slug}"
             )
 
--- crowdFunding/views.py
+++ crowdFunding/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render,get_object_or_404
+from django.utils import timezone
 from .models import Fund 
 
 def index(request):
@@ -6,13 +7,17 @@ def index(request):
         new_fund=Fund(
                 title=request.POST.get("title"),
                 content=request.POST.get("description"),
-                goal=request.POST.get("goal")
+                goal=request.POST.get("goal"),
+                event_datetime=timezone.now()
                 )
         new_fund.save()
         return redirect("cf-home")
     return render(request,"crowdFunding/index.html")
 
 def details(request,slug):
+    if request.method == "POST":
+        donation=request.POST.get("donation")
+
     fund = get_object_or_404(Fund, slug=slug)
     return render(request, 'crowdFunding/details.html', {'fund': fund})
 
